aae625b324b08d8ab175d7e846b9e2f219f275ad
TC <vnguye3@uw.edu>
2022-12-12 13:55:29 -0800

    Implemented total_page

--- procReport.c
+++ procReport.c
@@ -3,6 +3,38 @@
 #include<linux/pid_namespace.h>
 #include<asm/io.h>
 
+long virt2phys(struct mm_struct *mm, long vpage) {
+  long physical_page_addr = 0;
+  pgd_t *pgd;
+  p4d_t *p4d;
+  pud_t *pud;
+  pmd_t *pmd;
+  pte_t *pte;
+  struct page *page;
+  pgd = pgd_offset(mm, vpage);
+  if (pgd_none(*pgd) || pgd_bad(*pgd))
+    return 0;
+  p4d = p4d_offset(pgd, vpage);
+  if (p4d_none(*p4d) || p4d_bad(*p4d))
+    return 0;
+  pud = pud_offset(p4d, vpage);
+  if (pud_none(*pud) || pud_bad(*pud))
+    return 0;
+  pmd = pmd_offset(pud, vpage);
+  if (pmd_none(*pmd) || pmd_bad(*pmd))
+    return 0;
+  if (!(pte = pte_offset_map(pmd, vpage)))
+    return 0;
+  if (!(page = pte_page(*pte)))
+    return 0;
+  physical_page_addr = page_to_phys(page);
+  pte_unmap(pte);
+  if (physical_page_addr == 70368744173568)
+    return 0;
+  return physical_page_addr;
+}
+
+
 int proc_count(void){
   int i=0;
   struct task_struct *thechild;
@@ -11,7 +43,7 @@ int proc_count(void){
   return i;
 }
 
-int total_pages_calc(struct task_struct *thechild) {
+int total_pages_calc(struct task_struct *task) {
   // DEFINE total_pages 0
   // DEFINE start as vm_start
   // DEFINE end as vm_end
@@ -19,8 +51,15 @@ int total_pages_calc(struct task_struct *thechild) {
   //  if (virt2phys(i) != 0)
   //   total_pages++;
   // return total_pages
-
-  return 0;
+  int totalPageCount = 0;
+  struct vm_area_struct *vma = 0;
+  unsigned long vpage;
+  if (task->mm && task->mm->mmap)
+    for (vma = task->mm->mmap; vma; vma = vma->vm_next)
+      for (vpage = vma->vm_start; vpage < vma->vm_end; vpage += PAGE_SIZE)
+        if (virt2phys(task->mm, vpage) != 0)
+          totalPageCount++;
+  return totalPageCount;
 }
 
 void proc_print(void) {
@@ -28,13 +67,12 @@ void proc_print(void) {
   struct task_struct *thechild;
   printk(KERN_INFO "PROCESS REPORT:");
   printk(KERN_INFO "proc_id, proc_name, contig_pages, noncontig_pages, total_pages");
-  for_each_process(thechild){
-    if (thechild->pid > 650) {
-      // printk("%d, %s", thechild->pid, thechild->comm);
-      printk("%d, %s, %d", thechild->pid, thechild->comm,
-        total_pages_calc(thechild));
-    }
-  }
+  // for_each_process(thechild){
+  //   if (thechild->pid > 650) {
+  //     printk("%d, %s, %d", thechild->pid, thechild->comm,
+  //       total_pages_calc(thechild));
+  //   }
+  // }
   printk(KERN_INFO "TOTAL,,%d,%d,%d", 0, 0, 0);
 
 }
--- procReport.mod.c
+++ procReport.mod.c
@@ -29,12 +29,21 @@ static const struct modversion_info ____versions[]
 __used __section("__versions") = {
 	{ 0xf704969, "module_layout" },
 	{ 0x92997ed8, "_printk" },
-	{ 0x5b8239ca, "__x86_return_thunk" },
 	{ 0xab0324d4, "init_task" },
+	{ 0xa648e561, "__ubsan_handle_shift_out_of_bounds" },
+	{ 0x97651e6c, "vmemmap_base" },
+	{ 0x7cd8d75e, "page_offset_base" },
+	{ 0x4c55f5ef, "pv_ops" },
+	{ 0xdad13544, "ptrs_per_p4d" },
+	{ 0x8a35b432, "sme_me_mask" },
+	{ 0x1d19f77b, "physical_mask" },
+	{ 0x5b8239ca, "__x86_return_thunk" },
+	{ 0xa92ec74, "boot_cpu_data" },
+	{ 0x72d79d83, "pgdir_shift" },
 	{ 0xbdfb6dbb, "__fentry__" },
 };
 
 MODULE_INFO(depends, "");
 
 
-MODULE_INFO(srcversion, "8D68685E1AAFD1C6885EC2F");
+MODULE_INFO(srcversion, "F2BA38C3A700396B3D2B3AC");
